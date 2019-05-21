c9fe4a7f36c33eaaf8a6d3f7315eaa1ba6ad341e
peiqianlong <862896267@qq.com>
2019-05-21 14:45:02 +0800

    first-file

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-
-
-
-// 引入element
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.use(ElementUI)
-
-Vue.config.productionTip = false
-
-new Vue({
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import store from './store';
+import './mock/index.js';  // 该项目所有请求使用mockjs模拟
+import './login.js' 
+import './ui.js' 
+Vue.config.productionTip = false;
+var vm=new Vue({
+  el: '#app',
   router,
-  render: h => h(App)
-}).$mount('#app')
+  store,
+  template: '<App/>',
+  components: { App }
+})
+
+
